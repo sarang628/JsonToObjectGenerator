5303c01a275eee47ea15cebc78b1a26d34337e35
SRYANG <sarang628@naver.com>
2023-05-04 17:14:17 +0800

    Initial commit

---
+++ library/src/main/java/com/example/library/JsonToObjectGenerator.kt
@@ -0,0 +1,24 @@
+package com.example.library
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.reflect.Type
+
+class JsonToObjectGenerator<T> {
+    fun getListByFile(context: Context, fileName: String, rawType: Type): List<T> {
+        val inputStream = context.assets.open(fileName)
+        val inputStreamReader = InputStreamReader(inputStream)
+        val bufferReader = BufferedReader(inputStreamReader)
+
+        val gson = Gson();
+        TypeToken.getParameterized(List::class.java, rawType).type
+        val list = gson.fromJson<List<T>>(
+            bufferReader,
+            TypeToken.getParameterized(List::class.java, rawType).type
+        )
+        return list
+    }
+}
